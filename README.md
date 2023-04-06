598bb2e09d7b75f37c2afb18225f4a417e3c47a1
Jaxcoder <codenamejason@outlook.com>
2023-04-05 20:02:48 -0400

    add some config

--- README.md
+++ README.md
@@ -2,12 +2,27 @@
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
-Try running some of the following tasks:
+## Networks Supported
+- localhost/hardhat
+- mainnet
+- goerli
+- optimism
+- optimism goerli
+- fantom
+- fantom testnet
+- polygon
+- mumbai
+- zkSync era
+- zkSync era goerli
 
+
+
+Clone the repo
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
+git clone https://github.com/codenamejason/hardhat-contracts.git
+```
+Install dependencies
+```shell
+cd hardhat-contracts
+yarn install
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,42 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 
-const INFURA_KEY = process.env.INFURA_KEY;
+const INFURA_KEY = process.env.INFURA_KEY || "";
 const MNEMONIC = process.env.MNEMONIC || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
-      accounts: [MNEMONIC],
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        count: 20,
+        passphrase: "",
+      },
     },
   },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  }
 };
 
 export default config;
