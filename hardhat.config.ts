5d122de6389dd2f40e498382260b31517f1a3d8a
Jaxcoder <codenamejason@outlook.com>
2023-04-05 22:15:30 -0400

    add some more shit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,191 @@
-import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
 
-const INFURA_KEY = process.env.INFURA_KEY || "";
-const MNEMONIC = process.env.MNEMONIC || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-solhint";
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "@primitivefi/hardhat-dodoc";
+import "@typechain/hardhat";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+import "solidity-coverage";
+
+dotenv.config();
+
+const chainIds = {
+  // local
+  localhost: 31337,
+  // testnet
+  goerli: 5,
+  "fantom-testnet": 4002,
+  "zksync-testnet": 280,
+  mumbai: 80001,
+
+  // mainnet
+  mainnet: 1,
+  "optimism-mainnet": 10,
+  "fantom-mainnet": 250,
+  "zksync-mainnet": 324,
+  polygon: 137,
+};
+
+const infuraIdKey = process.env.INFURA_ID as string;
+let deployPrivateKey = process.env.DEPLOYER_PRIVATE_KEY as string;
+if (!deployPrivateKey) {
+  deployPrivateKey =
+    "0x0000000000000000000000000000000000000000000000000000000000000001";
+}
+
+/**
+ * Generates hardhat network configuration the test networks.
+ * @param network
+ * @param url (optional)
+ * @returns {NetworkUserConfig}
+ */
+function createTestnetConfig(
+  network: keyof typeof chainIds,
+  url?: string
+): NetworkUserConfig {
+  if (!url) {
+    url = `https://${network}.infura.io/v3/${infuraIdKey}`;
+  }
+  return {
+    accounts: [deployPrivateKey],
+    chainId: chainIds[network],
+    allowUnlimitedContractSize: true,
+    url,
+  };
+}
+
+/**
+ * Generates hardhat network configuration the mainnet networks.
+ * @param network
+ * @param url (optional)
+ * @returns {NetworkUserConfig}
+ */
+function createMainnetConfig(
+  network: keyof typeof chainIds,
+  url?: string
+): NetworkUserConfig {
+  if (!url) {
+    url = `https://${network}.infura.io/v3/${infuraIdKey}`;
+  }
+  return {
+    accounts: [deployPrivateKey],
+    chainId: chainIds[network],
+    url,
+  };
+}
+
+const abiExporter = [
+  {
+    path: "./abis/pretty",
+    flat: true,
+    clear: true,
+    format: "fullName",
+  },
+  {
+    path: "./abis/ugly",
+    flat: true,
+    clear: true,
+  },
+];
+
+const dodoc = {
+  outputDir: "./docs/contracts",
+  exclude: ["contracts/dummy", "contracts/mocks"],
+};
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        count: 20,
-        passphrase: "",
-      },
-    },
-  },
   solidity: {
-    version: "0.8.19",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 400,
       },
     },
   },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  }
+  networks: {
+    // Main Networks
+    mainnet: createMainnetConfig("mainnet"),
+    "optimism-mainnet": createMainnetConfig("optimism-mainnet"),
+    "fantom-mainnet": createMainnetConfig(
+      "fantom-mainnet",
+      "https://rpc.ftm.tools"
+    ),
+    "zksync-mainnet": {
+      ...createMainnetConfig(
+        "zksync-mainnet",
+        "https://zksync2-mainnet.zksync.io",
+      ), zksync: true, ethNetwork: "mainnet"
+    },
+    polygon: createMainnetConfig("polygon"),
+
+    // Test Networks
+    goerli: createTestnetConfig("goerli"),
+    "fantom-testnet": createTestnetConfig(
+      "fantom-testnet",
+      "https://rpc.testnet.fantom.network/"
+    ),
+    localhost: createTestnetConfig("localhost", "http://localhost:8545"),
+    hardhat: { zksync: true },
+    "zksync-testnet": {
+      ...createTestnetConfig(
+        "zksync-testnet",
+        "https://zksync2-testnet.zksync.dev"
+      ), zksync: true, ethNetwork: "goerli"
+    },
+    mumbai: createTestnetConfig("mumbai"),
+  },
+  defaultNetwork: "zksync-testnet",
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      // @ts-ignore
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      // @ts-ignore
+      goerli: process.env.ETHERSCAN_API_KEY,
+      // @ts-ignore
+      optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY,
+      // @ts-ignore
+      ftmTestnet: process.env.FTMSCAN_API_KEY,
+      // @ts-ignore
+      opera: process.env.FTMSCAN_API_KEY,
+    },
+  },
+  abiExporter: abiExporter,
+  dodoc: dodoc,
+  zksolc: {
+    version: "1.3.7",
+    compilerSource: "binary",
+    settings: {
+      isSystem: true,
+    },
+  },
 };
 
 export default config;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("blockNumber", "Prints the block number for mainnet", async (_, { ethers }) => {
+  const blockNumber = await ethers.provider.getBlockNumber();
+  console.log("Block Number: ", blockNumber);
+});
